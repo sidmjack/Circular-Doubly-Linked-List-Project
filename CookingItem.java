7025586eb26d09d6471f111dad57966840a82f04
Lawrence <lawrence@jhu.edu>
2016-02-15 13:00:19 -0500

    fixed indentation to use spaces, not tabs

--- CookingItem.java
+++ CookingItem.java
@@ -16,34 +16,34 @@
 
 public final class CookingItem implements CookingItemInterface {
  
- 	/** 
- 	 * Time Elapsed in Game. 
-	 */
-	private static int time_elapsed;
-	/** 
-	 * Cooking_Item Name.
-	 */
-	private String name;	
-	/** 
-	 * Cooking Time.
-	 */
-	private int cook_time;
-	/**
-	 *  Time left for item to cook.
-	 */
-	private int time_left;
-	/**
-	 * Last Time Item was Checked.
-	 */
-	private int last_check;
-	/** 
-	 * Penalty for Undercooked Items.
-	 */
-	private int underPenalty;
-	/** 
-	 * Penalty for Burnt Items.
-	 */
-	private int overPenalty;
+    /** 
+     * Time Elapsed in Game. 
+     */
+    private static int time_elapsed;
+    /** 
+     * Cooking_Item Name.
+     */
+    private String name;    
+    /** 
+     * Cooking Time.
+     */
+    private int cook_time;
+    /**
+     *  Time left for item to cook.
+     */
+    private int time_left;
+    /**
+     * Last Time Item was Checked.
+     */
+    private int last_check;
+    /** 
+     * Penalty for Undercooked Items.
+     */
+    private int underPenalty;
+    /** 
+     * Penalty for Burnt Items.
+     */
+    private int overPenalty;
 
     /**
      * Constructor for a CookingItem.
@@ -64,78 +64,78 @@ public final class CookingItem implements CookingItemInterface {
         this.overPenalty = burnPenalty;
     }
 
-	/**
-	 *  Cooking Item Object Constructor for when passed another item.
-	 *  @param item 		Already defined CookingItem Object
-	 */
-	CookingItem(CookingItem item) {
-		this.name = item.name;
-		this.cook_time = item.cook_time;
-		this.time_left = item.time_left;
-		this.underPenalty = item.underPenalty;
-		this.overPenalty = item.overPenalty;
-	}
+    /**
+     *  Cooking Item Object Constructor for when passed another item.
+     *  @param item         Already defined CookingItem Object
+     */
+    CookingItem(CookingItem item) {
+        this.name = item.name;
+        this.cook_time = item.cook_time;
+        this.time_left = item.time_left;
+        this.underPenalty = item.underPenalty;
+        this.overPenalty = item.overPenalty;
+    }
 
-	/**
-	 *  Cooking Item Object Constructor for when passed NULL.
-	 */
-	CookingItem() {
-		this.name = "NON_ITEM";
-		this.cook_time = 0;
-		this.time_left = 0;
-		this.underPenalty = 0;
-		this.overPenalty = 0;
-	}
-	
-	/** Updates the food item's fields based on the time-elapsed.
-	 *  Implements a simulation of one minute of time for this item 
+    /**
+     *  Cooking Item Object Constructor for when passed NULL.
+     */
+    CookingItem() {
+        this.name = "NON_ITEM";
+        this.cook_time = 0;
+        this.time_left = 0;
+        this.underPenalty = 0;
+        this.overPenalty = 0;
+    }
+    
+    /** Updates the food item's fields based on the time-elapsed.
+     *  Implements a simulation of one minute of time for this item 
      *  by decrementing cooking time by one minute.
-	 */
-	public void tick() {
-		cook_time--;
-	}
-	
-	/** Prints general item details...
-	 * Mostly just for testing purposes...
-	 */
-	public void print_item() {
-		System.out.println("Item Name: " + this.name + "\n");
-		System.out.println("Item Cook Time: " + this.time_left+ "\n");
-		System.out.println("Current Penalty: " + this.penalty() + "\n");
-		System.out.println("Time Remaining: " + this.timeRemaining() + "\n");		
-	}
+     */
+    public void tick() {
+        cook_time--;
+    }
+    
+    /** Prints general item details...
+     * Mostly just for testing purposes...
+     */
+    public void print_item() {
+        System.out.println("Item Name: " + this.name + "\n");
+        System.out.println("Item Cook Time: " + this.time_left+ "\n");
+        System.out.println("Current Penalty: " + this.penalty() + "\n");
+        System.out.println("Time Remaining: " + this.timeRemaining() + "\n");       
+    }
 
-	/**
-	 * Prints a string of the cooking item: Contaings Name and time_left.
-	 * @return str 	String of CookingItem
-	 */
-	public String toString(){
-		String str = "(" + this.name + " " + this.time_left + ")";
+    /**
+     * Prints a string of the cooking item: Contaings Name and time_left.
+     * @return str  String of CookingItem
+     */
+    public String toString(){
+        String str = "(" + this.name + " " + this.time_left + ")";
         return str;
-	}
+    }
 
    /**
     * Returns time remaining for this dish.
     * @return The amount of time left to cook.
     */
-	public int timeRemaining() {
-		return this.time_left; 
-	} 
-	
-	/** 
-	 * Returns the penalty if this dish were removed now.
-	 * Method Checks time remaining for item and imparts 
-	 * appropriate d.
-	 * @return The penalty for removing the CookingItem now
- 	 */
-	public int penalty() {
-		if (this.cook_time == 0) {
-			return 0;
-		} else if (this.timeRemaining() > 0) {
-			return this.underPenalty * this.cook_time;
-		} else {
-			return this.overPenalty * -this.cook_time;
-		}
-	}
-	
+    public int timeRemaining() {
+        return this.time_left; 
+    } 
+    
+    /** 
+     * Returns the penalty if this dish were removed now.
+     * Method Checks time remaining for item and imparts 
+     * appropriate d.
+     * @return The penalty for removing the CookingItem now
+     */
+    public int penalty() {
+        if (this.cook_time == 0) {
+            return 0;
+        } else if (this.timeRemaining() > 0) {
+            return this.underPenalty * this.cook_time;
+        } else {
+            return this.overPenalty * -this.cook_time;
+        }
+    }
+    
 }
