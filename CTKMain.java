56fea0dbb5fa9674248effe1989b8704b337d90e
Lawrence <lawrence@jhu.edu>
2016-02-17 01:01:14 -0500

    new line in sim files

--- CTKMain.java
+++ CTKMain.java
@@ -103,11 +103,11 @@ public final class CTKMain {
 
                 
                 while (!kitchen.isEmpty()) {
-                    writer.write(kitchen.toString());
+                    writer.write(kitchen.toString() + "\n");
                     kitchen.kitchenTick(); //Runs the tick method for the stations and their items.
                 }
 
-                writer.write(kitchen.toString());
+                writer.write(kitchen.toString() + "\n");
                 writer.close();
             } catch (IOException e) {
                 e.printStackTrace();
